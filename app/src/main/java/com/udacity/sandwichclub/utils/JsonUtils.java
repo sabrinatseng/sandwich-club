b4cbfd4cd2ce147b9d5d4aa90864f9588f57af08
Sabrina Tseng <stseng110499@gmail.com>
2018-04-28 18:21:42 -0400

    improvements

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -11,23 +11,30 @@ import java.util.List;
 
 public class JsonUtils {
 
+    public static final String KEY_MAIN_NAME = "mainName";
+    public static final String KEY_ALSO_KNOWN_AS = "alsoKnownAs";
+    public static final String KEY_PLACE_OF_ORIGIN = "placeOfOrigin";
+    public static final String KEY_DESCRIPTION = "description";
+    public static final String KEY_IMAGE = "image";
+    public static final String KEY_INGREDIENTS = "ingredients";
+
     public static Sandwich parseSandwichJson(String json) {
         try {
             JSONObject sandwich = new JSONObject(json);
             JSONObject name = sandwich.getJSONObject("name");
-            String mainName = name.getString("mainName");
-            JSONArray alsoKnownAsArray = name.getJSONArray("alsoKnownAs");
+            String mainName = name.optString(KEY_MAIN_NAME, "");
+            JSONArray alsoKnownAsArray = name.getJSONArray(KEY_ALSO_KNOWN_AS);
             List<String> alsoKnownAs = new ArrayList<String>();
 
             for (int i = 0; i < alsoKnownAsArray.length(); i++) {
                 alsoKnownAs.add(alsoKnownAsArray.getString(i));
             }
 
-            String placeOfOrigin = sandwich.getString("placeOfOrigin");
-            String description = sandwich.getString("description");
-            String image = sandwich.getString("image");
+            String placeOfOrigin = sandwich.optString(KEY_PLACE_OF_ORIGIN, "");
+            String description = sandwich.optString(KEY_DESCRIPTION, "");
+            String image = sandwich.optString(KEY_IMAGE, "");
 
-            JSONArray ingredientsArray = sandwich.getJSONArray("ingredients");
+            JSONArray ingredientsArray = sandwich.getJSONArray(KEY_INGREDIENTS);
             List<String> ingredients = new ArrayList<String>();
 
             for (int i = 0; i < ingredientsArray.length(); i++) {
